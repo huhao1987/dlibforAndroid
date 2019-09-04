8fb6e81cc94bbd35469bddc53ef9c8ecd89aea76
hh <huhao1987@gmail.com>
2019-09-04 22:51:07 +1000

    Update

--- app/src/main/cpp/at.cpp
+++ app/src/main/cpp/at.cpp
@@ -1,10 +1,14 @@
 #include <jni.h>
 #include <string>
 
+#include <native-lib.cpp>
+
+
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_au_hao_and_dlibforandroid_MainActivity_stringFroget(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from at";
+    std::string hello = matrix_ex();
     return env->NewStringUTF(hello.c_str());
 }
