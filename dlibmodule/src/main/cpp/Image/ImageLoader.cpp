aa6dd7e0d317c2ead5b72bf208de039717bc4ea6
Hao <huhao1987@gmail.com>
2019-09-10 16:51:12 +1000

    Update load_jpeg,load_bmp,load_image

--- dlibmodule/src/main/cpp/Image/ImageLoader.cpp
+++ dlibmodule/src/main/cpp/Image/ImageLoader.cpp
@@ -5,7 +5,9 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+
 #define DLIB_JPEG_SUPPORT
+
 #include <dlib/image_io.h>
 
 #include <android/log.h>
@@ -21,39 +23,57 @@ using namespace std;
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 
-jpeg_loader *jpegloader=NULL;
+jpeg_loader *jpegloader = NULL;
 #define JPEGNAME(NAME)\
     Java_au_hao_and_dlibmodule_Image_Jpegloader_##NAME
 
-    extern "C"
-    JNIEXPORT void JNICALL
-    JPEGNAME(initimage)(
-            JNIEnv *env,
-            jobject thiz,
-            jstring filename) {
-        std::string str = jstring2str(env,filename);
-        jpegloader = new jpeg_loader(str.c_str());
-    }
+extern "C"
+JNIEXPORT void JNICALL
+JPEGNAME(initimage)(
+        JNIEnv *env,
+        jobject thiz,
+        jstring filename) {
+    std::string str = jstring2str(env, filename);
+    jpegloader = new jpeg_loader(str.c_str());
+}
 extern "C"
 JNIEXPORT jboolean JNICALL
 JPEGNAME(isgray)(
         JNIEnv *env,
         jobject thiz) {
-        if(jpegloader==NULL)
+    if (jpegloader == NULL) {
         return NULL;
-        else{
-            return jpegloader->is_gray();
-        }
+    } else {
+        return jpegloader->is_gray();
+    }
 }
 extern "C"
 JNIEXPORT jboolean JNICALL
 JPEGNAME(isrgb)(
         JNIEnv *env,
         jobject thiz) {
-    if(jpegloader==NULL)
+    if (jpegloader == NULL)
         return NULL;
-    else{
+    else {
         return jpegloader->is_rgb();
+    }
+}
+
+dlib::array2d<dlib::rgb_pixel> getJpegimage(JNIEnv *env) {
+    if (jpegloader == NULL)
+        return NULL;
+    else {
+        dlib::array2d<dlib::rgb_pixel> arr2d;
+        jpegloader->get_image(arr2d);
+        return arr2d;
+    }
+}
+dlib::array2d<dlib::rgb_pixel> getJpegimage(JNIEnv *env,dlib::array2d<dlib::rgb_pixel> arr2d) {
+    if (jpegloader == NULL)
+        return NULL;
+    else {
+        jpegloader->get_image(arr2d);
+        return arr2d;
     }
 }
 
@@ -62,9 +82,19 @@ JNIEXPORT void JNICALL
 JPEGNAME(release)(
         JNIEnv *env,
         jobject thiz) {
-    if(jpegloader==NULL)
+    if (jpegloader == NULL)
         return;
-    else{
-        jpegloader=NULL;
+    else {
+        jpegloader = NULL;
     }
+}
+
+dlib::array2d<dlib::rgb_pixel> loadbmp(JNIEnv *env,dlib::array2d<dlib::rgb_pixel> arr2d, std::string filename) {
+    dlib::load_bmp(arr2d,filename.c_str());
+    return arr2d;
+}
+
+dlib::array2d<dlib::rgb_pixel> loadimage(JNIEnv *env,dlib::array2d<dlib::rgb_pixel> arr2d, std::string filename) {
+    dlib::load_image(arr2d,filename.c_str());
+    return arr2d;
 }
